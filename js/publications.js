38b7410623d456af3e0c767b93573d8c08928d68
Toni Penya-Alba <tonipenya@iiia.csic.es>
2013-02-18 11:18:47 +0100

    Deleted commented code.

--- js/publications.js
+++ js/publications.js
@@ -2,18 +2,6 @@ $(function() {
     $.get('publications.bib', function(result) {
         $('#bibtex_input').append(result);
         bibtex_js_draw();
-
-//        $('.expand').click(function(){
-//            $(this).hide();    
-//            $(this).siblings('.abstract').show();
-//            $(this).siblings('.collapse').show();
-//        });
-//
-//        $('.collapse').click(function(){
-//            $(this).hide();    
-//            $(this).siblings('.abstract').hide();
-//            $(this).siblings('.expand').show();
-//        });
     });
 
 });
